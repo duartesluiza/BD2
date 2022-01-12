626d0b1ded70e5bf82f2505e9e1d56d0bac7590e
duartesluiza <duartes.luiza92@gmail.com>
2022-01-12 19:56:09 -0300

    Subindo os 45 arquivos da lista de exercícios 1

--- exercicios-1/BD2_exercicios-1_45.sql
+++ exercicios-1/BD2_exercicios-1_45.sql
@@ -3,6 +3,7 @@
 # Da tabela cidade o nome, o distrito e a população. Da tabela língua do país o nome do idioma. Selecione
 # apenas os idiomas oficiais e os países que se encontram no continente ‘Europe’.
 
+
 USE world;
 
 SELECT  
